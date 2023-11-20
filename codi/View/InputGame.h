dfc599c2e91a46b55521e962f24713d26b18e6da
martibruix <martibruix02@gmail.com>
2023-11-20 08:42:08 +0100

    InputGame implementat

--- codi/View/InputGame.h
+++ codi/View/InputGame.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <string>
+#include <iostream>
 using namespace std;
 
 class InputGame
 {
 public:
 	virtual string getInstruccio() {
-		string missatge = "";
+		string missatge;
+		cin >> missatge;
 		return missatge;
 	}
 };
